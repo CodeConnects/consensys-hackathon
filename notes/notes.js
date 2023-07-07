aa07511aebbca66d68f6fc0e0f8f780b4a975360
Scottamoosh <4674926+CodeConnects@users.noreply.github.com>
2023-07-07 19:09:52 -0400

    add opening ceremony video link

--- notes/notes.js
+++ notes/notes.js
@@ -4,4 +4,7 @@ Key Dates:
   Hacking Period: July 6 - August 21
   Judging Period: August 21 - 30
   Closing Ceremony: September 6
+
+  Opening Ceremony video
+    https://www.youtube.com/watch?v=QbF9HCD_UUI
 */
