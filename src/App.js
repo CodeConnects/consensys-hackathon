ecea305839576066516006b4fd2e8e6b1e795029
CodeConnects <codeconnects@gmail.com>
2023-09-19 10:02:29 -0400

    add list of insect images and 2 raw buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>Swatter</p>
+        <h1>Swatter</h1>
+        
+        <h2>Who You Swattin?</h2>
+        <ul class="insects-list">
+          
+          <li class="insect">
+            <p>Mosquito</p>
+            <img src="https://pngimg.com/uploads/mosquito/small/mosquito_PNG18158.png" alt="mosquito" class="insect-img" />
+          </li>
+          
+          <li class="insect">
+            <p>Spider</p>
+            <img src="https://pngimg.com/uploads/spider/spider_PNG20.png" alt="spider" class="insect-img" />
+          </li>
+
+          <li class="insect">
+            <p>Fly</p>
+            <img src="https://pngimg.com/uploads/fly/fly_PNG3946.png" alt="fly" class="insect-img" />
+          </li>
+
+          <li class="insect">
+            <p>Roach</p>
+            <img src="https://pngimg.com/uploads/roach/roach_PNG12163.png" alt="roach" class="insect-img" />
+          </li>
+
+        </ul>
+        
+        <button className='btn' id='start-btn'>Start Swatting</button>
       </header>
     </div>
   );
