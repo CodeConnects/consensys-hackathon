07e1b4e63f0fb82e583073b04a805f7cb2a3b007
CodeConnects <codeconnects@gmail.com>
2023-09-20 12:47:57 -0400

    convert insects into array and map for li

--- src/components/ChooseInsect.js
+++ src/components/ChooseInsect.js
@@ -1,29 +1,37 @@
+
+const insects = [
+  {
+    name: 'Mosquito',
+    img: 'https://pngimg.com/uploads/mosquito/small/mosquito_PNG18158.png',
+    alt: 'mosquito'
+  },
+  {
+    name: 'Spider',
+    img: 'https://pngimg.com/uploads/spider/spider_PNG20.png',
+    alt: 'spider'
+  },
+  {
+    name: 'Fly',
+    img: 'https://pngimg.com/uploads/fly/fly_PNG3946.png',
+    alt: 'fly'
+  },
+  {
+    name: 'Roach',
+    img: 'https://pngimg.com/uploads/roach/roach_PNG12163.png',
+    alt: 'roach'
+  }
+];
+
 function ChooseInsect() {
   return (
-    <div className="choose-insect">
-      <h2>Who You Swattin?</h2>
-      <ul>
-        <li className="insect">
-          <p>Mosquito</p>
-          <img src="https://pngimg.com/uploads/mosquito/small/mosquito_PNG18158.png" alt="mosquito" className="insect-img" />
+    <ul>
+      {insects.map(insect => (
+        <li key={insect.name} className="insect">
+          <p>{insect.name}</p>
+          <img src={insect.img} alt={insect.alt} className="insect-img" />
         </li>
-        
-        <li className="insect">
-          <p>Spider</p>
-          <img src="https://pngimg.com/uploads/spider/spider_PNG20.png" alt="spider" className="insect-img" />
-        </li>
-
-        <li className="insect">
-          <p>Fly</p>
-          <img src="https://pngimg.com/uploads/fly/fly_PNG3946.png" alt="fly" className="insect-img" />
-        </li>
-
-        <li className="insect">
-          <p>Roach</p>
-          <img src="https://pngimg.com/uploads/roach/roach_PNG12163.png" alt="roach" className="insect-img" />
-        </li>
-      </ul>
-    </div>
+      ))}
+    </ul>
   );
 }
 
