3b5ce21c4cd908f2972a1533a1a483bb7862fb35
CodeConnects <codeconnects@gmail.com>
2023-09-20 13:16:17 -0400

    layout basic score and time styles for home screen

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,15 +1,17 @@
 function StatusBar() {
   return (
     <div className='status-bar'>
-      <div className='status-bar__item'>
-        <p className='status-bar__title'>Score</p>
-        <p className='status-bar__value' id='score'>0</p>
+      
+      <div className='status-bar-item'>
+        <p className='status-bar-title'>Score</p>
+        <p className='status-bar-value' id='score'>0</p>
       </div>
       
-      <div className='status-bar__item'>
-        <p className='status-bar__title'>Time</p>
-        <p className='status-bar__value' id='time'>0</p>
+      <div className='status-bar-item'>
+        <p className='status-bar-title'>Time</p>
+        <p className='status-bar-value' id='time'>0</p>
       </div>
+      
     </div>
   );
 }
