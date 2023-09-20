3e0614a1aef4089c366634a9f6504c52a2967a2a
CodeConnects <codeconnects@gmail.com>
2023-09-20 14:09:35 -0400

    move state from choose insect component to main app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import StatusBar from './components/StatusBar.js';
 import './styles/StatusBar.css';
 import ChooseInsect from './components/ChooseInsect.js';
@@ -5,7 +7,33 @@ import './styles/ChooseInsect.css';
 
 import './App.css';
 
+const insects = [
+  {
+    name: 'Mosquito',
+    img: 'https://pngimg.com/uploads/mosquito/small/mosquito_PNG18158.png',
+    alt: 'mosquito'
+  },
+  {
+    name: 'Spider',
+    img: 'https://pngimg.com/uploads/spider/spider_PNG20.png',
+    alt: 'spider'
+  },
+  {
+    name: 'Fly',
+    img: 'https://pngimg.com/uploads/fly/fly_PNG3946.png',
+    alt: 'fly'
+  },
+  {
+    name: 'Roach',
+    img: 'https://pngimg.com/uploads/roach/roach_PNG12163.png',
+    alt: 'roach'
+  }
+];
+
 function App() {
+  
+  const [activeInsect, setActiveInsect] = useState(null);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +41,11 @@ function App() {
 
         <StatusBar />
 
-        <ChooseInsect />
+        <ChooseInsect 
+          insects={insects} 
+          activeInsect={activeInsect} 
+          setActiveInsect={setActiveInsect}
+        />
         
         <button className='btn' id='start-btn'>Start Swatting</button>
       </header>
--- src/components/ChooseInsect.js
+++ src/components/ChooseInsect.js
@@ -1,31 +1,4 @@
-import React, { useState } from 'react';
-
-const insects = [
-  {
-    name: 'Mosquito',
-    img: 'https://pngimg.com/uploads/mosquito/small/mosquito_PNG18158.png',
-    alt: 'mosquito'
-  },
-  {
-    name: 'Spider',
-    img: 'https://pngimg.com/uploads/spider/spider_PNG20.png',
-    alt: 'spider'
-  },
-  {
-    name: 'Fly',
-    img: 'https://pngimg.com/uploads/fly/fly_PNG3946.png',
-    alt: 'fly'
-  },
-  {
-    name: 'Roach',
-    img: 'https://pngimg.com/uploads/roach/roach_PNG12163.png',
-    alt: 'roach'
-  }
-];
-
-function ChooseInsect() {
-
-  const [activeInsect, setActiveInsect] = useState(null);
+function ChooseInsect({ insects, activeInsect, setActiveInsect }) {
 
   return (
     <div className="choose-insect">
