95dddfc9176bd511e4d6975c46472f2552fcabca
CodeConnects <codeconnects@gmail.com>
2023-09-20 19:55:20 -0400

    add reset button and handler with animation classes

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,11 +1,35 @@
-function StatusBar({score, setScore, timer, setTimer}) {
+function StatusBar({score, setScore, timer, setTimer, setGameInsects, setGameOn}) {
+  
+  const handleReset = () => {
+    setScore(0);
+    setTimer(30);
+    document.querySelector('.choose-insect h2').classList.remove('hidden');
+    document.querySelector('#start-btn').classList.remove('hidden');
+    document.querySelector('.choose-insect li:nth-child(1)').classList.remove('hidden');
+    document.querySelector('.choose-insect li:nth-child(2)').classList.remove('hidden');
+    document.querySelector('.choose-insect li:nth-child(3)').classList.remove('hidden');
+    document.querySelector('.choose-insect li:nth-child(4)').classList.remove('hidden');
+
+    document.querySelector('h1').classList.remove('game-on');
+
+    document.querySelector('.status-bar').classList.remove('game-on');
+    document.querySelector('.status-bar').classList.add('game-off');
+
+    setGameInsects([]);
+    setGameOn(false);
+  }
+  
   return (
-    <div className='status-bar'>
+    <div className='status-bar game-off'>
       
       <div className='status-bar-item'>
         <p className='status-bar-title'>Score</p>
         <p className='status-bar-value' id='score'>{score}</p>
       </div>
+
+      <button className='btn' id='help-btn' onClick={() => alert('Help Text')}>Help</button>
+
+      <button className='btn' id='reset-btn' onClick={handleReset}>Reset</button>
       
       <div className='status-bar-item'>
         <p className='status-bar-title'>Time</p>
