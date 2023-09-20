728a47265d27eaa722d467b6761ec5e82ef3b953
CodeConnects <codeconnects@gmail.com>
2023-09-20 13:29:54 -0400

    add state handler for choosing an insect

--- src/components/ChooseInsect.js
+++ src/components/ChooseInsect.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 
 const insects = [
   {
@@ -23,12 +24,19 @@ const insects = [
 ];
 
 function ChooseInsect() {
+
+  const [activeInsect, setActiveInsect] = useState(null);
+
   return (
     <div className="choose-insect">
       <h2>Who You Swattin?</h2>
       <ul>
         {insects.map(insect => (
-          <li key={insect.name} className="insect">
+          <li
+            key={insect.name}
+            className={`insect ${insect.name === activeInsect ? 'active' : ''}`}
+            onClick={() => setActiveInsect(insect.name)}
+          >
             <p>{insect.name}</p>
             <img src={insect.img} alt={insect.alt} className="insect-img" />
           </li>
